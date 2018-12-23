c5faecb993980f21c2b29a9e96432edfdfad29d7
xiongwei08 <xiongweil@126.com>
2018-12-23 11:45:27 +0800

    create a project

---
+++ winio/src/main/java/winio/Test.java
@@ -0,0 +1,16 @@
+package winio;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+
+		DD.INSTANCE.DD_str("123456"); 
+		
+		for(int i=0; i<100; i++) {
+			//System.out.println("yp_phone_result_" + i);
+		}
+	}
+
+
+}
